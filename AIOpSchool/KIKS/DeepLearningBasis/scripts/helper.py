b6a39178297cb6aeb4dbdb51470f275de4f873c6
Zimcke Van de Staey <zimcke@gmail.com>
2022-03-18 10:47:03 +0100

    Uploading images and data

---
+++ AIOpSchool/KIKS/DeepLearningBasis/scripts/helper.py
@@ -0,0 +1,19 @@
+import ipywidgets as widgets
+import io
+import numpy as np
+
+upload_widget = widgets.FileUpload(
+    accept=".npy",
+    multiple=False  # We laden slechts één bestand op
+)
+
+def save_npy():
+    if not upload_widget.value:
+        print('No npy file selected yet.')
+        return None
+
+    first_key = next(iter(upload_widget.value))
+
+    eigen_npy = io.BytesIO(upload_widget.value[first_key]['content'])
+    eigen_afbeelding = np.load(eigen_npy)
+    np.save('../images/eigen_afbeelding.npy', eigen_afbeelding)
---
+++ STEM/DigitaleBeelden/scripts/helper.py
@@ -0,0 +1,36 @@
+import ipywidgets as widgets
+import io
+from PIL import Image
+import numpy as np
+
+upload_image_widget = widgets.FileUpload(
+    accept=".jpg, .jpeg",
+    multiple=False  # We laden slechts één bestand op
+)
+
+upload_npy_widget = widgets.FileUpload(
+    accept=".npy",
+    multiple=False  # We laden slechts één bestand op
+)
+
+def save_afbeelding():
+    if not upload_image_widget.value:
+        print('No image selected yet.')
+        return None
+
+    first_key = next(iter(upload_image_widget.value))
+
+    image = Image.open(io.BytesIO(upload_image_widget.value[first_key]['content']))
+    image.save('../images/eigen_afbeelding.jpg')
+    return image
+
+def save_npy():
+    if not upload_npy_widget.value:
+        print('No npy file selected yet.')
+        return None
+
+    first_key = next(iter(upload_npy_widget.value))
+
+    eigen_npy = io.BytesIO(upload_npy_widget.value[first_key]['content'])
+    eigen_afbeelding = np.load(eigen_npy)
+    np.save('../data/eigen_afbeelding.npy', eigen_afbeelding)
